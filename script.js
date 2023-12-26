e4efed46fef2d3da3ce4b7d6f065f861f4b1fff9
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-26 23:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var count=0;
+let value=document.getElementById("counter");
+let button=document.getElementsByClassName("incrementBtn").item(0);
+button.addEventListener('click',()=>{
+	alert(count++);
+	value.innerText=count;
+})
